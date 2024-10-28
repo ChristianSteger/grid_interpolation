211c55f09866e44534a7d6e09bbd75929f6a7bcb
Christian Steger <csteger@Christians-MacBook-Pro.local>
2024-10-28 23:18:48 +0100

    Fortran code style improved

--- idw_interp_esrg.f90
+++ idw_interp_esrg.f90
@@ -95,158 +95,158 @@ MODULE idw_interp_esrg
   ! Find nearest neighbours from grid cell centre and their distances
   ! ---------------------------------------------------------------------------
 
-	SUBROUTINE nearest_neighbours_esrg(points, &
-		index_of_pts, indptr, num_ppgc, num_nn, &
-		x_axis, y_axis, grid_spac, ind_x, ind_y, index_nn, dist_nn)
+  SUBROUTINE nearest_neighbours_esrg(points, &
+    index_of_pts, indptr, num_ppgc, num_nn, &
+    x_axis, y_axis, grid_spac, ind_x, ind_y, index_nn, dist_nn)
 
-		REAL, DIMENSION(:, :), INTENT(IN) :: points ! (# of pts, 2)
-		INTEGER, DIMENSION(:), INTENT(IN) :: index_of_pts ! (num_points)
-		INTEGER, DIMENSION(:, :), INTENT(IN) :: indptr ! (len_y, len_x)
-		INTEGER, DIMENSION(:, :), INTENT(IN) :: num_ppgc ! (len_y, len_x)
-		INTEGER, INTENT(IN) :: num_nn
-		REAL, DIMENSION(:), INTENT(IN) :: x_axis, y_axis
-		REAL, INTENT(IN) :: grid_spac
-		INTEGER, INTENT(IN) :: ind_x, ind_y
-		INTEGER, DIMENSION(:), INTENT(OUT) :: index_nn
-		REAL, DIMENSION(:), INTENT(OUT) :: dist_nn
+    REAL, DIMENSION(:, :), INTENT(IN) :: points ! (# of pts, 2)
+    INTEGER, DIMENSION(:), INTENT(IN) :: index_of_pts ! (num_points)
+    INTEGER, DIMENSION(:, :), INTENT(IN) :: indptr ! (len_y, len_x)
+    INTEGER, DIMENSION(:, :), INTENT(IN) :: num_ppgc ! (len_y, len_x)
+    INTEGER, INTENT(IN) :: num_nn
+    REAL, DIMENSION(:), INTENT(IN) :: x_axis, y_axis
+    REAL, INTENT(IN) :: grid_spac
+    INTEGER, INTENT(IN) :: ind_x, ind_y
+    INTEGER, DIMENSION(:), INTENT(OUT) :: index_nn
+    REAL, DIMENSION(:), INTENT(OUT) :: dist_nn
 
-		INTEGER :: len_x, len_y
-		REAL :: radius_sq, dist_sq
-		REAL, DIMENSION(:) :: dist_sq_nn(num_nn)
-		INTEGER :: i, j, k
-		INTEGER :: ind_x_nb, ind_y_nb
-		INTEGER :: ind, ind_is, level, num_nn_found
-		REAL, DIMENSION(:) :: centre(2)
-		TYPE(point_attr), DIMENSION(1000) :: points_cons
-		INTEGER :: num_pts_cons
-		TYPE(point_attr), DIMENSION(1000) :: points_cons_next
-		INTEGER :: num_pts_cons_next
+    INTEGER :: len_x, len_y
+    REAL :: radius_sq, dist_sq
+    REAL, DIMENSION(:) :: dist_sq_nn(num_nn)
+    INTEGER :: i, j, k
+    INTEGER :: ind_x_nb, ind_y_nb
+    INTEGER :: ind, ind_is, level, num_nn_found
+    REAL, DIMENSION(:) :: centre(2)
+    TYPE(point_attr), DIMENSION(1000) :: points_cons
+    INTEGER :: num_pts_cons
+    TYPE(point_attr), DIMENSION(1000) :: points_cons_next
+    INTEGER :: num_pts_cons_next
 
-		! Shape of regular grid
-		len_x = SIZE(x_axis)
-		len_y = SIZE(y_axis)
+    ! Shape of regular grid
+    len_x = SIZE(x_axis)
+    len_y = SIZE(y_axis)
 
-		! Initialise arrays
-		index_nn = -1
-		dist_sq_nn = HUGE(1.0)
+    ! Initialise arrays
+    index_nn = -1
+    dist_sq_nn = HUGE(1.0)
 
-		! Centre coordinates of the grid cell
-		centre(1) = x_axis(ind_x)
-		centre(2) = y_axis(ind_y)
+    ! Centre coordinates of the grid cell
+    centre(1) = x_axis(ind_x)
+    centre(2) = y_axis(ind_y)
 
-		! -------------------------------------------------------------------------
-		! Process centre grid cell
-		! -------------------------------------------------------------------------
+    ! -------------------------------------------------------------------------
+    ! Process centre grid cell
+    ! -------------------------------------------------------------------------
 
-		level = 0
-		radius_sq = (grid_spac * (level + 0.5)) ** 2
+    level = 0
+    radius_sq = (grid_spac * (level + 0.5)) ** 2
 
-		! Assign points to list
-		num_pts_cons = 0
-		DO i = 1, num_ppgc(ind_y, ind_x)
-			ind = index_of_pts(indptr(ind_y, ind_x) + (i - 1))
-			dist_sq = (centre(1) - points(ind, 1)) ** 2 &
-							+ (centre(2) - points(ind, 2)) ** 2
-			num_pts_cons = num_pts_cons + 1
-			points_cons(num_pts_cons)%index = ind
-			points_cons(num_pts_cons)%dist_sq = dist_sq
-		END DO
+    ! Assign points to list
+    num_pts_cons = 0
+    DO i = 1, num_ppgc(ind_y, ind_x)
+      ind = index_of_pts(indptr(ind_y, ind_x) + (i - 1))
+      dist_sq = (centre(1) - points(ind, 1)) ** 2 &
+              + (centre(2) - points(ind, 2)) ** 2
+      num_pts_cons = num_pts_cons + 1
+      points_cons(num_pts_cons)%index = ind
+      points_cons(num_pts_cons)%dist_sq = dist_sq
+    END DO
 
-		! Potentially add new points
-		num_pts_cons_next = 0
-		DO i = 1, num_pts_cons
-			ind = points_cons(i)%index
-			dist_sq = points_cons(i)%dist_sq
-			IF (dist_sq > radius_sq) THEN
-				num_pts_cons_next = num_pts_cons_next + 1
-				points_cons_next(num_pts_cons_next)%index = ind
-				points_cons_next(num_pts_cons_next)%dist_sq = dist_sq
-			ELSE IF (dist_sq < dist_sq_nn(num_nn)) THEN
-				ind_is = count(dist_sq_nn < dist_sq) + 1 ! insertion index
-				dist_sq_nn(ind_is + 1:num_nn) = dist_sq_nn(ind_is:num_nn - 1)
-				dist_sq_nn(ind_is) = dist_sq
-				index_nn(ind_is + 1:num_nn) = index_nn(ind_is:num_nn - 1)
-				index_nn(ind_is) = ind
-			END IF
-		END DO
+    ! Potentially add new points
+    num_pts_cons_next = 0
+    DO i = 1, num_pts_cons
+      ind = points_cons(i)%index
+      dist_sq = points_cons(i)%dist_sq
+      IF (dist_sq > radius_sq) THEN
+        num_pts_cons_next = num_pts_cons_next + 1
+        points_cons_next(num_pts_cons_next)%index = ind
+        points_cons_next(num_pts_cons_next)%dist_sq = dist_sq
+      ELSE IF (dist_sq < dist_sq_nn(num_nn)) THEN
+        ind_is = count(dist_sq_nn < dist_sq) + 1 ! insertion index
+        dist_sq_nn(ind_is + 1:num_nn) = dist_sq_nn(ind_is:num_nn - 1)
+        dist_sq_nn(ind_is) = dist_sq
+        index_nn(ind_is + 1:num_nn) = index_nn(ind_is:num_nn - 1)
+        index_nn(ind_is) = ind
+      END IF
+    END DO
 
-		num_nn_found = count(index_nn /= -1)
+    num_nn_found = count(index_nn /= -1)
 
-		! -------------------------------------------------------------------------
-		! Process grid cells of surrounding 'frame(s)'
-		! -------------------------------------------------------------------------
+    ! -------------------------------------------------------------------------
+    ! Process grid cells of surrounding 'frame(s)'
+    ! -------------------------------------------------------------------------
 
-		DO WHILE (num_nn_found /= num_nn)
+    DO WHILE (num_nn_found /= num_nn)
 
-			level = level + 1
-			radius_sq = (grid_spac * (level + 0.5)) ** 2
+      level = level + 1
+      radius_sq = (grid_spac * (level + 0.5)) ** 2
 
-			! Assign points from bottom and top cells to list
-			points_cons(:num_pts_cons_next) = points_cons_next(:num_pts_cons_next)
-			num_pts_cons = num_pts_cons_next
-			DO i = -level, level, 2 * level ! y-axis
-				DO j = -level, level ! x-axis
-					ind_y_nb = ind_y + i
-					ind_x_nb = ind_x + j
-					IF ((ind_x_nb < 1) .OR. (ind_x_nb > len_x) .OR. &
-							(ind_y_nb < 1) .OR. (ind_y_nb > len_y)) THEN
-						CYCLE
-					END IF
-					DO k = 1, num_ppgc(ind_y_nb, ind_x_nb)
-						ind = index_of_pts(indptr(ind_y_nb, ind_x_nb) + (k - 1))
-						dist_sq = (centre(1) - points(ind, 1)) ** 2 &
-										+ (centre(2) - points(ind, 2)) ** 2
-						num_pts_cons = num_pts_cons + 1
-						points_cons(num_pts_cons)%index = ind
-						points_cons(num_pts_cons)%dist_sq = dist_sq
-					END DO
-				END DO
-			END DO
-
-			! Assign points from left and right cells to list
-			DO j = -level, level, 2 * level ! x-axis
-				DO i = -level + 1, level - 1 ! y_axis
-					ind_y_nb = ind_y + i
-					ind_x_nb = ind_x + j
-					IF ((ind_x_nb < 1) .OR. (ind_x_nb > len_x) .OR. &
-							(ind_y_nb < 1) .OR. (ind_y_nb > len_y)) THEN
-						CYCLE
-					END IF
-					DO k = 1, num_ppgc(ind_y_nb, ind_x_nb)
-						ind = index_of_pts(indptr(ind_y_nb, ind_x_nb) + (k - 1))
-						dist_sq = (centre(1) - points(ind, 1)) ** 2 &
-										+ (centre(2) - points(ind, 2)) ** 2
-						num_pts_cons = num_pts_cons + 1
-						points_cons(num_pts_cons)%index = ind
-						points_cons(num_pts_cons)%dist_sq = dist_sq          
+      ! Assign points from bottom and top cells to list
+      points_cons(:num_pts_cons_next) = points_cons_next(:num_pts_cons_next)
+      num_pts_cons = num_pts_cons_next
+      DO i = -level, level, 2 * level ! y-axis
+        DO j = -level, level ! x-axis
+          ind_y_nb = ind_y + i
+          ind_x_nb = ind_x + j
+          IF ((ind_x_nb < 1) .OR. (ind_x_nb > len_x) .OR. &
+            (ind_y_nb < 1) .OR. (ind_y_nb > len_y)) THEN
+            CYCLE
+          END IF
+          DO k = 1, num_ppgc(ind_y_nb, ind_x_nb)
+            ind = index_of_pts(indptr(ind_y_nb, ind_x_nb) + (k - 1))
+            dist_sq = (centre(1) - points(ind, 1)) ** 2 &
+                    + (centre(2) - points(ind, 2)) ** 2
+            num_pts_cons = num_pts_cons + 1
+            points_cons(num_pts_cons)%index = ind
+            points_cons(num_pts_cons)%dist_sq = dist_sq
           END DO
         END DO
       END DO
 
-			! Potentially add new points
-			num_pts_cons_next = 0
-			DO i = 1, num_pts_cons
-				ind = points_cons(i)%index
-				dist_sq = points_cons(i)%dist_sq
-				IF (dist_sq > radius_sq) THEN
-					num_pts_cons_next = num_pts_cons_next + 1
-					points_cons_next(num_pts_cons_next)%index = ind
-					points_cons_next(num_pts_cons_next)%dist_sq = dist_sq
-				ELSE IF (dist_sq < dist_sq_nn(num_nn)) THEN
-					ind_is = count(dist_sq_nn < dist_sq) + 1 ! insertion index
-					dist_sq_nn(ind_is + 1:num_nn) = dist_sq_nn(ind_is:num_nn - 1)
-					dist_sq_nn(ind_is) = dist_sq
-					index_nn(ind_is + 1:num_nn) = index_nn(ind_is:num_nn - 1)
-					index_nn(ind_is) = ind
-				END IF
-			END DO
+      ! Assign points from left and right cells to list
+      DO j = -level, level, 2 * level ! x-axis
+        DO i = -level + 1, level - 1 ! y_axis
+          ind_y_nb = ind_y + i
+          ind_x_nb = ind_x + j
+          IF ((ind_x_nb < 1) .OR. (ind_x_nb > len_x) .OR. &
+              (ind_y_nb < 1) .OR. (ind_y_nb > len_y)) THEN
+            CYCLE
+          END IF
+          DO k = 1, num_ppgc(ind_y_nb, ind_x_nb)
+            ind = index_of_pts(indptr(ind_y_nb, ind_x_nb) + (k - 1))
+            dist_sq = (centre(1) - points(ind, 1)) ** 2 &
+                    + (centre(2) - points(ind, 2)) ** 2
+            num_pts_cons = num_pts_cons + 1
+            points_cons(num_pts_cons)%index = ind
+            points_cons(num_pts_cons)%dist_sq = dist_sq          
+          END DO
+        END DO
+      END DO
 
-			num_nn_found = count(index_nn /= -1)
+      ! Potentially add new points
+      num_pts_cons_next = 0
+      DO i = 1, num_pts_cons
+        ind = points_cons(i)%index
+        dist_sq = points_cons(i)%dist_sq
+        IF (dist_sq > radius_sq) THEN
+          num_pts_cons_next = num_pts_cons_next + 1
+          points_cons_next(num_pts_cons_next)%index = ind
+          points_cons_next(num_pts_cons_next)%dist_sq = dist_sq
+        ELSE IF (dist_sq < dist_sq_nn(num_nn)) THEN
+          ind_is = count(dist_sq_nn < dist_sq) + 1 ! insertion index
+          dist_sq_nn(ind_is + 1:num_nn) = dist_sq_nn(ind_is:num_nn - 1)
+          dist_sq_nn(ind_is) = dist_sq
+          index_nn(ind_is + 1:num_nn) = index_nn(ind_is:num_nn - 1)
+          index_nn(ind_is) = ind
+        END IF
+      END DO
 
-		END DO
+      num_nn_found = count(index_nn /= -1)
 
-		dist_nn = SQRT(dist_sq_nn)
-    
-	END SUBROUTINE nearest_neighbours_esrg
+    END DO
+
+    dist_nn = SQRT(dist_sq_nn)
+
+  END SUBROUTINE nearest_neighbours_esrg
 
 END MODULE idw_interp_esrg
